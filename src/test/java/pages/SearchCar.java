09098d868d7a7caf219af4ffff56315205bc5157
Furkan Ozturk <furkanozturk1988@gmail.com>
2021-01-16 09:45:31 -0600

    add changes

--- src/test/java/pages/SearchCar.java
+++ src/test/java/pages/SearchCar.java
@@ -14,15 +14,36 @@ public class SearchCar {
 		
 		
 		
+
 		@FindBy (id = "zip")
 		public WebElement zip;
 		
 		@FindBy (xpath = "//div[@class='search-bar--form-field radius']")
 		public WebElement radius;
 		
-		@FindBy (xpath = "(//button[@class='save-car'])[2]")
+		@FindBy (xpath = "(//button[@aria-label='Save this car'])[1]")
 		public WebElement savedCar;
 		
+		@FindBy (xpath = "//button[@class='kmx-button kmx-button--secondary save-search--link']")
+		public WebElement savedSearch;
+		
+		@FindBy (xpath = "//input[@id='text-field-save-search-name']")
+		public WebElement savedSearchInput;
+		
+		@FindBy (xpath = "//button[@class='save-search-button kmx-button kmx-button--primary']")
+		public WebElement saveButton;
+		
+		@FindBy (xpath = "//button[@class='kmx-button kmx-button--tertiary save-search-success-modal--close']")
+		public WebElement gotIt;
+		
+		@FindBy (xpath = "//h2[@id='save-search-success-modal--header']")
+		public WebElement searchSavedMessage;
+		
+		@FindBy (xpath = "//button[@class='close-accessible-button base-modal--close']")
+		public WebElement alert;
+		
+		@FindBy (xpath = "(//button[@class='kmx-button kmx-button--tertiary kmx-button--flat'])[1]")
+		public WebElement keepStoreButton;
 		
 		
 		
---
+++ src/test/java/tests/SavedSearchTest.java
@@ -0,0 +1,71 @@
+package furkan;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import pageClasses.AfterSign;
+import pageClasses.MainPage;
+import pageClasses.SearchCar;
+import pageClasses.SignIn;
+import utilities.BrowserUtils;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class SavedSearchTest extends TestBase {
+	
+	
+	
+	@Test
+	public void savedSearch() {
+	
+		SoftAssert soft = new SoftAssert();
+		Driver.getDriver().get(ConfigReader.getProperty("url"));
+		MainPage mp = new MainPage();
+		mp.profileIcon.click();
+		BrowserUtils.waitForClickablility(mp.signIn, 10);
+		jsExecutor.executeScript("arguments[0].click();" , mp.signIn);
+		SignIn s = new SignIn();
+		s.userEmail.sendKeys(ConfigReader.getProperty("email"));
+		s.userPassword.sendKeys(ConfigReader.getProperty("password"));
+		s.signInButton.click();
+		BrowserUtils.waitFor(10);
+		
+		mp.mainSearchBox.sendKeys(ConfigReader.getProperty("car"),Keys.ENTER);
+		SearchCar sc = new SearchCar();
+		
+		
+		try{BrowserUtils.waitForClickablility(sc.keepStoreButton, 10);
+		jsExecutor.executeScript("arguments[0].click();", sc.keepStoreButton);}
+		catch(Exception e) {}
+		
+		
+		
+		sc.zip.click();
+		sc.zip.sendKeys(Keys.BACK_SPACE);
+		sc.zip.sendKeys("76210");
+		sc.savedSearch.click();
+		BrowserUtils.waitForClickablility(sc.saveButton, 5);
+		sc.saveButton.click();
+		String expectedMessage = "Search saved";
+		String actualMessage = sc.searchSavedMessage.getText();
+		soft.assertTrue(expectedMessage.contains(actualMessage));
+		BrowserUtils.waitFor(5);
+		sc.gotIt.click();
+
+		Driver.getDriver().get(ConfigReader.getProperty("url"));
+		AfterSign as = new AfterSign();
+		as.profileIcon.click();
+		as.savedSearches.click();
+		String car = Driver.getDriver().findElement(By.xpath("//h3[@class='kmx-typography--display-1']")).getText();
+		System.out.println(car);
+		soft.assertTrue(car.contains(ConfigReader.getProperty("car")));
+	
+	
+	
+	}
+}
